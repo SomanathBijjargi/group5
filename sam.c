46753268eccc3ad38641881423e367ecdaf1d58e
Duddagi <radhikadudagi@gmail.com>
2024-01-22 10:05:49 +0530

    sam file

---
+++ sam.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter a number");
+scanf("%d",&a);
+printf("enter b value");
+scanf("%d",&b);
+c=a+b;
+printf("result of %d c",&c);
+getch();
+}
+
